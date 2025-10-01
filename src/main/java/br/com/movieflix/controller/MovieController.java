b6eed2def8a3315525f3143996de730b5b70b0f4
RiquelSantos <riquesantosoliveira@gmail.com>
2025-10-01 09:12:03 -0300

    feat: Adicionando swagger da MovieController MovieRequest e MovieResponse

--- src/main/java/br/com/movieflix/controller/MovieController.java
+++ src/main/java/br/com/movieflix/controller/MovieController.java
@@ -5,6 +5,13 @@ import br.com.movieflix.controller.response.MovieResponse;
 import br.com.movieflix.entity.Movie;
 import br.com.movieflix.mapper.MovieMapper;
 import br.com.movieflix.service.MovieService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,23 +23,38 @@ import java.util.Optional;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/movieflix/movie")
+@Tag(name = "Movie", description = "Recurso responsavel pelo gerenciamento dos filmes.")
 public class MovieController {
 
     private final MovieService movieService;
 
+    @Operation(summary = "Salvar filme", description = "Método responsável por realizar o salvamento do novo filme",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponse(responseCode = "201", description = "Filme salvo com sucesso",
+    content = @Content(schema = @Schema(implementation = MovieResponse.class)))
     @PostMapping
     public ResponseEntity<MovieResponse> save(@Valid @RequestBody MovieRequest request){
         Movie savedMovie = movieService.save(MovieMapper.toMovie(request));
         return ResponseEntity.ok(MovieMapper.toMovieResponse(savedMovie));
     }
 
+    @Operation(summary = "Buscar filme", description = "Método responsável por retornar todos os filmes cadastrados.",
+    security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponse(responseCode = "200", description = "Retornar todos os filmes cadastrados.",
+            content = @Content(array = @ArraySchema(schema = @Schema(implementation = MovieResponse.class))))
     @GetMapping
     public ResponseEntity<List<MovieResponse>> findAll(){
-        return ResponseEntity.ok(movieService.findAll().stream()
+        return ResponseEntity.ok(movieService.findAll()
+                .stream()
                 .map(MovieMapper::toMovieResponse)
                 .toList());
     }
 
+    @Operation(summary = "Buscar filme por id", description = "Método responsável por buscar filme por id.",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponse(responseCode = "200", description = "Filme encontrado com sucesso",
+            content = @Content(schema = @Schema(implementation = MovieResponse.class)))
+    @ApiResponse(responseCode = "404", description = "Filme não encontrado", content = @Content())
     @GetMapping("/{id}")
     public ResponseEntity<MovieResponse> findMovieById(@PathVariable Long id){
         return movieService.findMovieById(id)
@@ -40,6 +62,11 @@ public class MovieController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Atualizar filme por id", description = "Método responsável por atualizar filme por id.",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponse(responseCode = "200", description = "Filme atualizado com sucesso",
+            content = @Content(schema = @Schema(implementation = MovieResponse.class)))
+    @ApiResponse(responseCode = "404", description = "Filme não encontrado", content = @Content())
     @PutMapping("/{id}")
     public ResponseEntity<MovieResponse> updateMovie(@PathVariable Long id, @Valid @RequestBody MovieRequest movieRequest){
         return movieService.update(id, MovieMapper.toMovie(movieRequest))
@@ -47,6 +74,10 @@ public class MovieController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Buscar filme por categoria", description = "Método responsável por retornar todos os filmes de uma categoria.",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponse(responseCode = "200", description = "Filmes encontrados com sucesso.",
+            content = @Content(array = @ArraySchema(schema = @Schema(implementation = MovieResponse.class))))
     @GetMapping("/search")
     public ResponseEntity<List<MovieResponse>> findByCategory (@RequestParam Long category){
         return ResponseEntity.ok(
@@ -57,6 +88,10 @@ public class MovieController {
         );
     }
 
+    @Operation(summary = "Deletar filme por id", description = "Método responsável por deletar filme por id.",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponse(responseCode = "204", description = "Filme deletado com sucesso", content = @Content())
+    @ApiResponse(responseCode = "404", description = "Filme não encontrado", content = @Content())
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteMovie(@PathVariable Long id){
         Optional<Movie> optMovie = movieService.findMovieById(id);
--- src/main/java/br/com/movieflix/controller/request/MovieRequest.java
+++ src/main/java/br/com/movieflix/controller/request/MovieRequest.java
@@ -1,6 +1,7 @@
 package br.com.movieflix.controller.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 
@@ -8,11 +9,18 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Builder
-public record MovieRequest(@NotEmpty(message = "Título do filme é obrigatório.") String title,
+public record MovieRequest(@Schema(type = "string", description = "Nome do filme")
+                           @NotEmpty(message = "Título do filme é obrigatório.")
+                           String title,
+                           @Schema(type = "string", description = "Descrição do filme")
                            String description,
                            @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+                           @Schema(type = "date", description = "Data de lançamento do filme. ex: '19/10/1998'")
                            LocalDate releaseDate,
+                           @Schema(type = "double", description = "Score do filme. ex: '7.8'")
                            double rating,
+                           @Schema(type = "array", description = "Lista o código de categorias")
                            List<Long> categories,
+                           @Schema(type = "array", description = "Lista o código de serviços de streaming")
                            List<Long> streamings) {
 }
