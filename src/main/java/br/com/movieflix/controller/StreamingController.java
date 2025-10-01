93d4c653d7cc143ec1b04fa776e5dbf87f44543c
RiquelSantos <riquesantosoliveira@gmail.com>
2025-10-01 09:32:06 -0300

    feat: Adiciona swagger na StreamingController

--- src/main/java/br/com/movieflix/controller/StreamingController.java
+++ src/main/java/br/com/movieflix/controller/StreamingController.java
@@ -1,10 +1,17 @@
 package br.com.movieflix.controller;
 
 import br.com.movieflix.controller.request.StreamingRequest;
+import br.com.movieflix.controller.response.CategoryResponse;
 import br.com.movieflix.controller.response.StreamingResponse;
 import br.com.movieflix.entity.Streaming;
 import br.com.movieflix.mapper.StreamingMapper;
 import br.com.movieflix.service.StreamingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,6 +27,10 @@ public class StreamingController {
 
     final private StreamingService streamingService;
 
+    @Operation(summary = "Buscar streaming", description = "Método responsável por retornar todos os streamings cadastrados.",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponse(responseCode = "200", description = "Retornar todas os streamings cadastrados.",
+            content = @Content(array = @ArraySchema(schema = @Schema(implementation = StreamingResponse.class))))
     @GetMapping()
     public ResponseEntity<List<StreamingResponse>> getAllStreaming(){
         List<StreamingResponse> streamings = streamingService.findAll()
@@ -30,6 +41,10 @@ public class StreamingController {
         return ResponseEntity.ok(streamings);
     }
 
+    @Operation(summary = "Salvar streaming", description = "Método responsável por realizar o salvamento da novo streaming",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponse(responseCode = "201", description = "Streaming salvo com sucesso",
+            content = @Content(schema = @Schema(implementation = StreamingResponse.class)))
     @PostMapping()
     public ResponseEntity<StreamingResponse> saveStreaming(@Valid @RequestBody StreamingRequest request){
         Streaming newStreaming = StreamingMapper.toStreaming(request);
@@ -37,6 +52,11 @@ public class StreamingController {
         return ResponseEntity.status(HttpStatus.CREATED).body(StreamingMapper.toStreamingResponse(savedStreaming));
     }
 
+    @Operation(summary = "Buscar streaming por id", description = "Método responsável por buscar streaming por id.",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponse(responseCode = "200", description = "Streaming encontrado com sucesso",
+            content = @Content(schema = @Schema(implementation = StreamingResponse.class)))
+    @ApiResponse(responseCode = "404", description = "Streaming não encontrado", content = @Content())
     @GetMapping("/{id}")
     public ResponseEntity<StreamingResponse> getByCategoryId(@PathVariable Long id){
         return streamingService.findById(id)
@@ -44,6 +64,10 @@ public class StreamingController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Deletar streaming por id", description = "Método responsável por deletar streaming por id.",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponse(responseCode = "204", description = "Streaming deletado com sucesso", content = @Content())
+    @ApiResponse(responseCode = "404", description = "Streaming não encontrado", content = @Content())
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteByStreamingId(@PathVariable Long id){
         streamingService.deleteStreaming(id);
