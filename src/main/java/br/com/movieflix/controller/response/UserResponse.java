ce6216feeb0c5584cfc2881b3eb22edb45151893
RiquelSantos <riquesantosoliveira@gmail.com>
2025-10-01 09:20:14 -0300

    feat: Adicionando swagger de todas as responses

--- src/main/java/br/com/movieflix/controller/response/UserResponse.java
+++ src/main/java/br/com/movieflix/controller/response/UserResponse.java
@@ -1,9 +1,13 @@
 package br.com.movieflix.controller.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
-public record UserResponse(Long id,
+public record UserResponse(@Schema(type = "long", description = "Código do usuário")
+                           Long id,
+                           @Schema(type = "string", description = "Nome do usuário")
                            String name,
+                           @Schema(type = "string", description = "Email do usuário")
                            String email) {
 }
